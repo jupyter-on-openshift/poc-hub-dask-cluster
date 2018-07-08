c6249cb03e3783d1092af82ec5f3666315d1383b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-07-08 11:26:16 +0800

    Initial files for dask cluster manager.

---
+++ cluster/wsgi.py
@@ -0,0 +1,38 @@
+import json
+import os
+from urllib.parse import quote
+
+from flask import Flask, redirect, request, Response
+
+from jupyterhub.services.auth import HubAuth
+
+prefix = os.environ.get('JUPYTERHUB_SERVICE_PREFIX', '/')
+
+auth = HubAuth(api_token=os.environ['JUPYTERHUB_API_TOKEN'],
+        cookie_cache_max_age=60)
+
+app = Flask(__name__)
+
+@wrapt.decorator
+def authenticated(wrapped, instance, args, kwargs):
+    cookie = request.cookies.get(auth.cookie_name)
+    token = request.headers.get(auth.auth_header_name)
+
+    if cookie:
+        user = auth.user_for_cookie(cookie)
+    elif token:
+        user = auth.user_for_token(token)
+    else:
+        user = None
+
+    if user:
+        return wrapped(user, *args, **kwargs)
+    else:
+        # Request to login url on failed authentication.
+        return redirect(auth.login_url + '?next=%s' % quote(request.path))
+
+@app.route(prefix)
+@authenticated
+def whoami(user):
+    return Response(json.dumps(user, indent=1, sort_keys=True),
+            mimetype='application/json')
